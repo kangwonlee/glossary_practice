9e7564599a48d4e3eb1e6333e527e66844ece8af
KangWon LEE <kangwon@gmail.com>
2023-08-04 15:25:18 +0900

    multiple choice

--- practice.py
+++ practice.py
@@ -16,16 +16,34 @@ def main(argv):
 
     for word in words:
         v = random.randint(0, 1)
+
+        three_others = random.sample(words, 3)
+
+        choices = list(set([word] + three_others))
+
+        random.shuffle(choices)
+
         if v:
             print(word)
+            print('=' * 20)
+            for j, w in enumerate(choices):
+                print(j+1, glossary[w])
             input()
+
+            print('#' * 20)
             print(glossary[word])
-            input()
         else:
             print(glossary[word])
+            print('=' * 20)
+            for j, w in enumerate(choices):
+                print(j+1, w)
+                print()
             input()
+
+            print('#' * 20)
             print(word)
-            input()
+
+        print('#' * 20)
 
 
 def get_flat_glossary(argv:List[str]) -> dict:
